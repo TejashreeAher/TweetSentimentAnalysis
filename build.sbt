bfa4fdcb1e2e3430af2251ced0ecd0164e2fbaa9
Tejashree Aher <tejashree.aher@xing.com>
2018-05-09 00:06:55 +0200

    Set correct jar name, dependency changes

--- build.sbt
+++ build.sbt
@@ -5,5 +5,11 @@ val root = project
     name := "tweet-sentiment-anaysis",
     version := "0.0.1",
     scalaVersion := "2.11.11",
-    libraryDependencies ++= Dependencies.root
+    libraryDependencies ++= Dependencies.root,
+    assemblyMergeStrategy in assembly := {
+    case PathList("log4j.xml", xs @ _ *)      => MergeStrategy.discard
+    case PathList("reference.conf", xs @ _ *) => MergeStrategy.concat
+    case PathList("META-INF", xs @ _ *)       => MergeStrategy.discard
+    case x                                    => MergeStrategy.first
+  }
   )
--- src/main/scala/com/job/models/Tweet.scala
+++ src/main/scala/com/job/models/Tweet.scala
@@ -1,10 +1,10 @@
 package com.job.models
 //likes is Option[Int] because otherwise 0 likes give non-nullable error
 case class Tweet(tweet_id: String, createdOn: String, text: String, likes: Option[Int])
-object Tweet{
-    def toString(tweet : Tweet)={
-      s"Tweet id : ${tweet.tweet_id}, created_on: ${tweet.createdOn}, text: ${tweet.text}, likes: ${tweet.likes}"
-    }
-}
-
 case class TweetSentiment(tweet_id: String, text: String, createdOn: String, likes: Option[Int], sentiment : Int)
+
+object Tweet{
+  def toString(tweet : Tweet)={
+    s"Tweet id : ${tweet.tweet_id}, created_on: ${tweet.createdOn}, text: ${tweet.text}, likes: ${tweet.likes}"
+  }
+}
