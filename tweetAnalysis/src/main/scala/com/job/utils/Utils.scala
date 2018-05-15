cb5dc2adcb7ffaa5379952c78030b53737dfd41d
Tejashree Aher <tejashree.aher@xing.com>
2018-05-15 23:35:31 +0200

    removed unwanted code

--- tweetAnalysis/src/main/scala/com/job/utils/Utils.scala
+++ tweetAnalysis/src/main/scala/com/job/utils/Utils.scala
@@ -1,4 +1,4 @@
-package utils
+package com.job.utils
 
 import java.text.SimpleDateFormat
 import java.time.LocalDate
--- tweetAnalysis/src/test/scala/UtilsSpec.scala
+++ tweetAnalysis/src/test/scala/UtilsSpec.scala
@@ -1,10 +1,10 @@
 import java.time.LocalDate
 
 import org.scalatest.FunSuite
-import utils.Utils
+import com.job.utils.Utils
 
-class UtilsSpec extends FunSuite{
-  test("twitter parser should parse date from response correctly"){
+class UtilsSpec extends FunSuite {
+  test("twitter parser should parse date from response correctly") {
     val date = "Wed Aug 27 13:08:45 +0000 2008"
     val parseddate = Utils.parseDate(date)
     val expectedDate = LocalDate.of(2008, 8, 27).toString
